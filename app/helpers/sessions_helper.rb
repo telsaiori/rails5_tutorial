537ef075f330510f633b865058e16cddb6943544
telsa <telsaiori.git@gmail.com>
2016-11-26 17:54:34 +0800

    add Friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,11 @@ module SessionsHelper
     end
   end
 
+
+  def current_user?(user)
+    user == current_user
+  end
+
   def logged_in?
     !current_user.nil?
   end
@@ -38,5 +43,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+    
 
 end
