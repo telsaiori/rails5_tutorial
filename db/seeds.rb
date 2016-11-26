2e6d9e97c955be16eba33c42c4c90236bb04cd01
telsa <telsaiori.git@gmail.com>
2016-11-26 19:56:38 +0800

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 User.create!(name:  "Telsa",
              email: "telsaiori@gmail.com",
              password:              "123456",
-             password_confirmation: "123456")
+             password_confirmation: "123456",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
