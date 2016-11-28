b2dffc4a39ea0275e59284d61a40f862d0a289c8
telsa <telsaiori.git@gmail.com>
2016-11-28 17:55:06 +0800

    finish email avtivation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App !"
-      log_in @user
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render action: 'new'
     end
