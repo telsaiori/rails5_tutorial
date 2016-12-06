a43403d0beef88a259d2de417f54315fc9181728
telsa <telsaiori.git@gmail.com>
2016-12-06 17:56:36 +0800

    add relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationships",
+                                  foreign_key: 'followed_id',
+                                  dependent: :destroy
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
