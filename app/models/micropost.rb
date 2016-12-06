793d8b96153453ae2ad9e3986350096c1641b21b
telsa <telsaiori.git@gmail.com>
2016-12-06 15:48:51 +0800

    add picture upload method

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,16 @@
 class Micropost < ApplicationRecord
+  mount_uploader :picture, PictureUploader
   belongs_to :user
   default_scope -> { order( created_at: :desc )}
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        erros.add(:picture, "should be less then 5mb")
+      end
+    end
 end
